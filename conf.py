99c954c09bc598a75759715603854ea9deabdf7a
Harshal Dhir <harshaldhir@ip-192-168-1-40.ec2.internal>
2020-05-16 14:51:55 -0700

    updating the readme

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ copyright = '2020, MessengerX.io'
 author = 'Harshal Dhir'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2-beta'
+release = '0.1.5'
 
 
 # -- General configuration ---------------------------------------------------
