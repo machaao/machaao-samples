53e254ec97ea3a5cbd49d2fec98349c42054e8da
Harshal Dhir <harshaldhir@ip-192-168-1-40.ec2.internal>
2020-07-14 08:36:00 -0700

    updating for latedt android sdk

--- basic_sample_bot/client/android/sample_bot_client/sample_bot_client/src/main/java/com/machaao/sample/generic/SplashActivity.java
+++ basic_sample_bot/client/android/sample_bot_client/sample_bot_client/src/main/java/com/machaao/sample/generic/SplashActivity.java
@@ -1,27 +1,77 @@
 package com.machaao.sample.generic;
 
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.machaao.android.sdk.Machaao;
 import com.machaao.android.sdk.activities.SingleBotActivity;
+import com.machaao.android.sdk.helpers.LogUtils;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = "SplashActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Start home activity
-        if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
-                Object value = getIntent().getExtras().get(key);
-                Log.d(SplashActivity.class.toString(), "Key: " + key + " Value: " + value);
-            }
+    public void onResume() {
+
+        if (!Machaao.isIntialized()) {
+            LogUtils.d(TAG, "waiting for initialization of SDK...");
+            LocalBroadcastManager.getInstance(this).registerReceiver(mInitialized,
+                    new IntentFilter("machaao.sdk.initialized"));
+        } else {
+            launch();
         }
 
-        startActivity(new Intent(SplashActivity.this, SingleBotActivity.class));
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mInitialized);
+        super.onDestroy();
+    }
+
+    private BroadcastReceiver mInitialized = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, final Intent intent) {
+            LogUtils.d(TAG, "received intent -> " + intent.toString());
+            launch();
+        }
+    };
+
+    private void launch() {
+        Intent intent = null;
+        try {
+            ApplicationInfo app = getApplication().getPackageManager().getApplicationInfo(getApplication().getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = app.metaData;
+
+            final String registeredClass = bundle.getString("com.machaao.android.sdk.single.activity.registered");
+
+            if(!StringUtils.isEmpty(registeredClass)) {
+                intent = new Intent(this, Class.forName(registeredClass));
+            } else {
+                intent = new Intent(this, SingleBotActivity.class);
+            }
+
+            intent.putExtra("botToken", Machaao.getApiToken());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(intent != null) {
+            startActivity(intent);
+        }
+
         // close splash activity
         finish();
     }
